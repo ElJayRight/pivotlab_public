8d499b6f9c30af5fd10effae1ee025aa52a0dcdd
eljayright <liamwri2003@hotmail.com>
2023-08-28 21:25:31 +1000

    Cleaned up the install process.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt update -y
-sudo apt install python3-pip -y
-sudo apt install python3-venv proxychains4 -y
+sudo apt install python3-pip proxychains4 docker.io docker-compose -y
+pip3 install virtualenv docker
 python3 -m virtualenv .venv
